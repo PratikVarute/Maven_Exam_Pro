6112c1f1058088546bc801b98bbbca7f487411ae
Pratik <pratikvarute07@gmail.com>
2022-03-04 15:26:45 +0530

    SDM EXAM Commit 1

---
+++ src/main/java/com/app/Maven_Exam_Pro/App.java
@@ -0,0 +1,13 @@
+package com.app.Maven_Exam_Pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
